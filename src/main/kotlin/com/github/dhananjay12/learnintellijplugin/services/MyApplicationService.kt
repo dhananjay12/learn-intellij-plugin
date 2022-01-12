02bf768a8babfe1be019b686ea52b774ce1970ab
GitHub Action <action@github.com>
2022-01-12 07:36:37 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/dhananjay12/learnintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.dhananjay12.learnintellijplugin.services
+
+import com.github.dhananjay12.learnintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
